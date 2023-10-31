dbd8853decf2a7aaf15ec44a0906921631b04a2d
ariolaqeleposhi <ariola.qeleposhi@replgroup.com>
2023-10-31 21:38:42 +0000

    Example of failing sanity test

---
+++ tests/sanity/failingTest.test.js
@@ -0,0 +1,34 @@
+const { Builder, By, Key, until } = require('selenium-webdriver')
+const firefox = require('selenium-webdriver/firefox');
+const assert = require('assert')
+const screen = {
+  width: 640,
+  height: 480
+};
+
+describe('@Sanity - Failing Test', function() {
+  this.timeout(30000)
+  let driver
+  let vars
+  beforeEach(async function() {
+    driver = new Builder()
+      .forBrowser('firefox')
+      .setFirefoxOptions(new firefox.Options().headless().windowSize(screen))
+      .build();
+    vars = {}
+  })
+  afterEach(async function() {
+    await driver.quit();
+  })
+  it('should fail if home page title is not correct', async function() {
+    await driver.get("https://www.saucedemo.com/")
+    await driver.manage().window().setRect({ width: 1300, height: 692 })
+    assert(await driver.findElement(By.css(".login_logo")).getText() == "Swag Labs")
+    await driver.findElement(By.css("*[data-test=\"username\"]")).click()
+    await driver.findElement(By.css("*[data-test=\"username\"]")).sendKeys("standard_user")
+    await driver.findElement(By.css("*[data-test=\"password\"]")).click()
+    await driver.findElement(By.css("*[data-test=\"password\"]")).sendKeys("secret_sauce")
+    await driver.findElement(By.css("*[data-test=\"login-button\"]")).click()
+    assert(await driver.findElement(By.css(".title")).getText() == "Productzz")
+  })
+})
